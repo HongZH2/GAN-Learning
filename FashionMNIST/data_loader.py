0bcedf044e93e5189fcfcf1cc397f57d8c14bbec
Hong <45353034+HongZH2@users.noreply.github.com>
2022-06-15 00:02:43 +0800

    my first network

--- FashionMNIST/data_loader.py
+++ FashionMNIST/data_loader.py
@@ -1,30 +1,32 @@
 import torch
 from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-from torchvision import transforms, datasets
+from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
 from torchvision.io import read_image
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-"""
-Fashion MNIST Dataset Class
-"""
+
 class MNISTDataset(Dataset):
+    """
+    Fashion MNIST Dataset Class
+    """
     def __init__(self, path, download=True, trans_func=ToTensor(), label_func=Lambda(lambda x: torch.zeros(10, dtype=torch.float).scatter_(0,torch.tensor(x), value=1))):
         # 1. download the dataset
         self.train_data = datasets.FashionMNIST(
             root = path,
             train = True,
             download = download,
-            transform = trans_func
+            transform = trans_func,
+            target_transform = label_func
         )
         self.test_data = datasets.FashionMNIST(
             root = path,
             train = False,
             download = download,
-            transform = trans_func
+            transform = trans_func,
+            target_transform=label_func
         )
         self.transform = trans_func
         self.label_transform = label_func
@@ -47,23 +49,23 @@ class MNISTDataset(Dataset):
             9: "Ankle Boot",
         }
 
-    """
-    brief: to query the size of dataset
-    """
     def __len__(self):
+        """
+        : return length the dataset
+        """
         return len(self.labels)
 
-    """
-    brief: to get one item from the dataset
-    """
     def __getitem__(self, item):
+        """
+        brief: to get one item from the dataset
+        """
         img_name, img_label = self.labels.iloc[item]
         img_path = os.path.join(self.img_path, img_name)
         img_data = read_image(img_path)
         if self.transform:
             img_data = self.transform(img_data)
         if self.label_transform:
-            img_label = self.label_tranform(img_label)
+            img_label = self.label_transform(img_label)
         return img_data, img_label
 
     def getTrainData(self):
@@ -73,13 +75,18 @@ class MNISTDataset(Dataset):
         return self.test_data
 
     def randomShowImages(self, n):
+        '''
+        Randomly pick images and print them out
+        :param n: n-by-n layouts
+        :return
+        '''
         figure = plt.figure(figsize=(8, 8))
         cols, rows = n, n
         for i in range(1, cols * rows + 1):
             sample_idx = torch.randint(len(self.train_data), size=(1,)).item()
             img, label = self.train_data[sample_idx]
             figure.add_subplot(rows, cols, i)
-            plt.title(self.labels_map[label])
+            plt.title(self.labels_map[list(label).index(1)])
             plt.axis("off")
             plt.imshow(img.squeeze(), cmap="gray")
         plt.show()
--- FashionMNIST/main.py
+++ FashionMNIST/main.py
@@ -2,12 +2,36 @@
 A demo for Neural Network Learning
 by Hong Zhang
 """
-import os
-from data_loader import MNISTDataset
 
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from data_loader import MNISTDataset
+from mNetwork import MNeuralNetwork
 
 if __name__ == '__main__':
+
+    # check if gpu is available
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
     # step 1. initialize the dataset
-    dataset = MNISTDataset('./', download = False)
+    dataset = MNISTDataset('./', download=False)
     # step 2. show some samples from Dataset
-    dataset.randomShowImages(3)
+    # dataset.randomShowImages(5)
+
+
+    # step 3. batch the dataset
+    batch_size = 64
+    train_loader = DataLoader(dataset.getTrainData(), batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset.getTestData(), batch_size=batch_size, shuffle=True)
+
+    # step 4. initialize my network
+    mnetwork = MNeuralNetwork().to(device)
+    print(mnetwork)
+
+    X = torch.rand(1, 28, 28, device=device)
+    logits = mnetwork(X)
+    pred_probab = nn.Softmax(dim=1)(logits)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted class: {y_pred}")
